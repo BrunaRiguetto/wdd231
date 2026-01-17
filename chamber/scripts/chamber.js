fc9f660154d3936e55181aef7b858bc59fcb60fb
BrunaRiguetto <bdonascimentovasconc@byupathway.edu>
2026-01-17 17:13:38 -0300

    directory creation

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,68 @@
+const membersContainer = document.querySelector("#members");
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+
+// Footer dates
+document.getElementById("lastModified").textContent = document.lastModified;
+
+// Fetch Members Data
+const url = "data/members.json";
+
+async function getMembers() {
+  const response = await fetch(url);
+  const data = await response.json();
+  displayMembers(data.members);
+}
+
+getMembers();
+
+// Display Members
+function displayMembers(members) {
+
+  members.forEach(member => {
+
+    const card = document.createElement("section");
+    card.classList.add("member-card");
+
+    const name = document.createElement("h3");
+    name.textContent = member.name;
+
+    const address = document.createElement("p");
+    address.textContent = member.address;
+
+    const phone = document.createElement("p");
+    phone.textContent = member.phone;
+
+    const website = document.createElement("a");
+    website.href = member.website;
+    website.textContent = member.website;
+    website.target = "_blank";
+
+    const image = document.createElement("img");
+    image.src = `images/${member.image}`;
+    image.alt = member.name;
+    image.loading = "lazy";
+
+    card.appendChild(name);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(website);
+    card.appendChild(image);
+
+
+    membersContainer.appendChild(card);
+  });
+}
+
+// Toggle View Buttons
+gridButton.addEventListener("click", () => {
+  membersContainer.classList.add("grid");
+  membersContainer.classList.remove("list");
+});
+
+listButton.addEventListener("click", () => {
+  membersContainer.classList.add("list");
+  membersContainer.classList.remove("grid");
+});
+
+document.getElementById("year").textContent = new Date().getFullYear();
