f6d9f8708e75ec64bf77e6ad821087b87a887818
BrunaRiguetto <bdonascimentovasconc@byupathway.edu>
2026-02-07 19:13:39 -0300

    join page

---
+++ chamber/data/discover.mjs
@@ -0,0 +1,50 @@
+export const places = [
+  {
+    name: "Igapó Lake",
+    address: "Av. Higienópolis, Londrina - PR",
+    description: "One of Londrina’s most iconic outdoor areas, ideal for walking, sports, and family activities.",
+    image: "images/londrina-1.webp"
+  },
+  {
+    name: "Zerão Park",
+    address: "Rua Maranhão, Londrina - PR",
+    description: "A large green park popular for jogging, cycling, and outdoor events.",
+    image: "images/zerao.webp"
+  },
+  {
+    name: "Londrina Museum",
+    address: "Rua Benjamin Constant, 900",
+    description: "Preserves the history and culture of Londrina and northern Paraná.",
+    image: "images/museum.webp"
+  },
+  {
+    name: "Cathedral of Londrina",
+    address: "Av. Paraná, 76",
+    description: "Modern architectural landmark and religious center of the city.",
+    image: "images/cathedral.webp"
+  },
+  {
+    name: "Botanical Garden",
+    address: "Av. dos Expedicionários",
+    description: "A peaceful area focused on environmental education and native plants.",
+    image: "images/botanical.webp"
+  },
+  {
+    name: "Catuai Shopping",
+    address: "Av. Ayrton Senna, 400",
+    description: "Major shopping center with restaurants, cinemas, and stores.",
+    image: "images/catuai.webp"
+  },
+  {
+    name: "'Coffee Stadium' (Jacy Scaff Stadium)",
+    address: "Av. Henrique Mansano, 889 - Alpes",
+    description: "A football stadium.",
+    image: "images/stadium.webp"
+  },
+  {
+    name: "Historic Coffee Museum",
+    address: "Rua Sergipe, Londrina",
+    description: "Highlights the coffee heritage that shaped the region’s economy.",
+    image: "images/coffeemuseum.webp"
+  }
+];
